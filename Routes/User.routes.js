4f0fe2916e6e09767349de57c1e67b8f6be7f286
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-12-06 21:05:53 +0530

    Updates

--- Routes/User.routes.js
+++ Routes/User.routes.js
@@ -1,11 +1,30 @@
 import { Router } from "express";
 import { Cart, addCart, deleteCart } from "../Controllers/User.controllers.js";
+import axios from "axios";
 
 const router = Router();
 
 router.post('/add-cart', addCart)
 router.post('/cart', Cart)
 router.post('/delete-cart', deleteCart)
+router.get('/test', async (req, res) => {
+    try {
+        const response = await axios.get('https://fakestoreapi.com/products')
+        // console.log(response.data)
+
+        // payment gateway
+        // kyc 
+        // chatbot 
+
+        // microservices - small servers
+
+        return res.status(200).json({ success: true, products: response.data })
+
+    } catch (error) {
+        console.log(error, "error")
+        return res.status(500).json({ success: false, message: error })
+    }
+})
 
 
 export default router;
--- Routes/index.js
+++ Routes/index.js
@@ -8,9 +8,9 @@ const router = Router();
 
 // axios.post('/api/v1/cart/addToCart', { productId, userId })
 
-router.use("/auth", authRoutes)
-router.use("/product", productRoutes)
-router.use("/user", userRoutes)
+router.use("/auth", authRoutes) // 8000 - backend-auth
+router.use("/product", productRoutes)  // 8001 backend-product
+router.use("/user", userRoutes) // // 8002 backend-user
 // router.use("/cart", productRoutes)
 // router.use("/product", productRoutes)
 // router.use("/product", productRoutes)
@@ -18,3 +18,19 @@ router.use("/user", userRoutes)
 // router.use("/product", productRoutes)
 
 export default router;
+
+
+
+
+// Lets say there are 5 products remanining in data, but 1000 of 
+// users are buying it, requesting for it, 
+
+// load balancer
+
+// 1. microservice - convert server into multiple servers
+
+// 2. mulitple servers - multiple servers * 10
+
+// 3. nginx - load balancer - aws 
+
+// 4. healthy and unhealthy, - nginx - configure
